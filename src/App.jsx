fd8711e89b140abfbbd2ea07274ca46854cb6a68
unknown <jithinbp007@gmail.com>
2024-04-14 14:28:39 +0530

    deploying with netlify

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,24 @@ import IpDetails from "./components/IpDetails";
 import Map from "./components/Map";
 
 const App = () => {
-  const [ip, setIP] = useState("");
+  const [ipAddress, setIPAddress] = useState("");
   const [details, setDetails] = useState([]);
   const [position, setPosition] = useState([]);
 
+  const checkIpAddress =
+    /^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/gi;
+  const checkDomain =
+    /^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+/;
+
+  const apiKey = import.meta.env.VITE_API_KEY;
+
   useEffect(() => {
+    const localIP = import.meta.env.VITE_IP_URL;
     async function getIP() {
       try {
-        const response = await fetch("https://api.ipify.org/?format=json");
+        const response = await fetch(`${localIP}`);
         const { ip } = await response.json();
-        setIP(ip);
+        setIPAddress(ip);
       } catch (error) {
         console.log(error);
       }
@@ -23,31 +31,39 @@ const App = () => {
 
   useEffect(() => {
     async function getDetails() {
-      if (!ip) return;
+      if (!ipAddress) return;
 
       try {
-        const response = await fetch(`http://ip-api.com/json/${ip}`);
-        const { query, city, regionName, lat, lon, timezone, isp } =
-          await response.json();
+        const response = await fetch(
+          `https://geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&${
+            checkIpAddress.test(ipAddress)
+              ? `ipAddress=${ipAddress}`
+              : checkDomain.test(ipAddress)
+              ? `domain=${ipAddress}`
+              : ""
+          }`
+        );
 
-        const currentLocation = city + "," + regionName;
+        const { ip, location, isp } = await response.json();
+
+        const currentLocation = location.city + "," + location.region;
         setDetails([
-          { title: "IP ADDRESS", data: query },
+          { title: "IP ADDRESS", data: ip },
           { title: "LOCATION", data: currentLocation },
-          { title: "TIMEZONE", data: timezone },
+          { title: "TIMEZONE", data: location.timezone },
           { title: "ISP", data: isp },
         ]);
 
-        setPosition([lat, lon]);
+        setPosition([location.lat, location.lng]);
       } catch (error) {
         console.log(error);
       }
     }
     getDetails();
-  }, [ip]);
+  }, [ipAddress]);
 
   const handleInputChange = (input) => {
-    setIP(input);
+    setIPAddress(input);
   };
 
   const handlePositionChange = (value) => {
